54b72c37d3c8b325dc0f05b1dd31b069538b1048
Pomodoro <penneconpomodoro@gmail.com>
2022-11-02 14:38:51 +0900

    タイトル画面で設定パネルを開いている時、2秒周期で音量確認用のSEが鳴るようにした

--- Assets/Scripts/ConfigurationPanelManager.cs
+++ Assets/Scripts/ConfigurationPanelManager.cs
@@ -5,9 +5,12 @@ using UnityEngine.UI;
 
 public class ConfigurationPanelManager : MonoBehaviour
 {
+    private float counter;
+
     // Start is called before the first frame update
     void Start()
     {
+        counter = 0f;
         transform.Find("BgmSlider").GetComponent<Slider>().value = PlayerDataManager.instance.bgmVolume;
         transform.Find("SeSlider").GetComponent<Slider>().value = PlayerDataManager.instance.seVolume;
     }
@@ -15,6 +18,12 @@ public class ConfigurationPanelManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        counter += Time.deltaTime;
+        if (counter > 2f)
+        {
+            counter -= 2f;
+            SoundManager.instance.PlaySE(SoundManager.SeType.GameStart);
+        }
         PlayerDataManager.instance.bgmVolume = (int)transform.Find("BgmSlider").GetComponent<Slider>().value;
         SoundManager.instance.bgmVolume = PlayerDataManager.instance.bgmVolume;
         PlayerDataManager.instance.seVolume = (int)transform.Find("SeSlider").GetComponent<Slider>().value;
